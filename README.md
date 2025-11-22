1cdf471c8f1688150794f30b1e9eb8d962e16ab1
CorbanPendrak <corbanpendrak@gmail.com>
2025-11-22 10:50:27 -0500

    Create mockup

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # InkChat
-
 This is a JavaScript library/tool to include chat functionality on a website powered by the Ink markdown language.
 
-## Todo
+## Naming
+Naming important things is fun, but here is the mapping:
+Paper -> Conversation window
+InkBlot -> Message in a conversation
 
+## Todo
 - [x] Setup repo
-- [ ] Create mockup
+- [x] Create mockup
+- [ ] Reset and close buttons
 - [ ] Style mockup
+  - [ ] Avatar
 - [ ] Add animations
+  - [ ] Paper expanding
+  - [ ] InkBlot typing
+  - [ ] Loading for long InkBlots?
+  - [ ] Scrolling
 - [ ] Improve styling configuration
 - [ ] Configure webpack
 - [ ] Connect Ink backend
 - [ ] Implement easy installation
   - [ ] Library
   - [ ] CDN
+- [ ] Convert to Typescript
--- index.js
+++ index.js
@@ -1,1 +1,136 @@
-alert("JavaScript is connected!");
+// InkChat code
+const titleText = "InkChat";
+
+function updateMessages(newMessages, messagesElement) {
+    for (var i = 0; i < newMessages.length; i++) {
+        console.log(((newMessages[i].isButton) ? "- " : "") + newMessages[i].text);
+        messagesElement.appendChild(newMessages[i].element);
+    }
+    messagesElement.scrollTop = messagesElement.scrollHeight;
+};
+
+function createInkBlot(text, messages=[]) {
+    const element = document.createElement("div");
+    element.textContent = text;
+    element.classList.add("box");
+
+    var isButton = false;
+    if (messages.length) {
+        isButton = true;
+        element.classList.add("button");
+        element.style = "width: fit-content; \
+            max-width: 95%; \
+            padding: 1em; \
+            margin-bottom: 0.5em; \
+            align-self: flex-end;";
+        element.addEventListener("click", () => {
+            if (isButton) {
+                // Remove other user options
+                var sibling = element.nextSibling;
+                while (sibling) {
+                    if (!sibling.classList.contains("button")) {
+                        break;
+                    }
+                    element.parentElement.removeChild(sibling);
+                    sibling = sibling.nextSibling;
+                }
+
+                var sibling = element.previousSibling;
+                while (sibling) {
+                    if (!sibling.classList.contains("button")) {
+                        break;
+                    }
+                    element.parentElement.removeChild(sibling);
+                    sibling = element.previousSibling;
+                }
+
+                updateMessages(messages, element.parentElement);
+                element.classList.remove("button");
+                isButton = false;
+            }
+        });
+    } else {
+        element.style = "width: fit-content; \
+            max-width: 95%; \
+            padding: 1em; \
+            margin-bottom: 0.5em;";
+    }
+
+    return {text, element, isButton};
+}
+
+
+// Create paper
+const body = document.querySelector("body");
+const paper = document.createElement("div");
+paper.classList.add("box");
+paper.style = "width: min(500px, 90vw); \
+    height: min(500px, 90vh); \
+    position: fixed; \
+    bottom: 16px; \
+    right: 16px; \
+    z-index: 1000; \
+    background-color: #fff; \
+    display: flex; \
+    flex-direction: column;";
+body.appendChild(paper);
+
+// Title
+const title = document.createElement("div");
+title.textContent = titleText;
+title.style = "text-align: center; \
+    font-weight: 700; \
+    font-size: 1.5em; \
+    padding-bottom: 0.5em;";
+paper.appendChild(title);
+
+// Separation bar
+const bar = document.createElement("hr");
+bar.style = "border-radius: 0; \
+    border: 2px solid #000; \
+    margin-bottom: 10px;";
+paper.appendChild(bar);
+
+// Message area
+var newMessages = [];
+const messagesElement = document.createElement("div");
+messagesElement.classList.add("box");
+messagesElement.style = "overflow: auto; \
+    flex-grow: 1; \
+    margin-bottom: 0; \
+    display: flex; \
+    flex-direction: column;";
+paper.appendChild(messagesElement);
+
+
+// Add intro message
+var messages = [];
+newMessages.push(createInkBlot("Welcome to InkBlot!"));
+newMessages.push(createInkBlot("This is a small demo of the chat application feature. Currently, it is not connected to Ink, so everything is hardcoded. That, however, will change soon!"));
+newMessages.push(createInkBlot("Wow, cool!", [
+    createInkBlot("I know, right?"), 
+    createInkBlot("Wait, so are you some kind of AI?", [
+        createInkBlot("No, I am hardcoded into this chat interface, so I am not AI."),
+        createInkBlot("Maybe someone wrote this flow with AI, but this conversation flow was designed to help answer your questions about this awesome website."),
+        createInkBlot("Oh, I guess that makes since.", [
+            createInkBlot("But I haven't gotten many options...", [
+                createInkBlot("Well, this is still in development. :)")
+            ]),
+        ]),
+    ]),
+]));
+newMessages.push(createInkBlot("Eh, I have seen this stuff before.", [
+    createInkBlot("Sure, you have probably seen chat interfaces on many websites, \
+        especially ones that can connect you with a \"helpful assistant\", but I am something different. \
+        I am powered by the powerful markdown like language of Ink, allowing a more conversational narrative \
+        to the website."),
+    createInkBlot("Sure, whatever.", [
+        createInkBlot("..."),
+    ]),
+    createInkBlot("I guess this is kind of interesting.", [
+        createInkBlot("Exactly! This allows for developers to create a chat application with a carefully \
+            worded flow for their website!"),
+    ])
+]));
+
+updateMessages(newMessages, messagesElement);
